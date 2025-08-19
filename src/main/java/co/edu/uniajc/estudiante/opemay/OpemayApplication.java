fcd24a200bebbc4f4c64700bb3e4a6c69b073b77
canarvaezis <criz.narvaez21@gmail.com>
2025-08-18 20:12:18 -0500

    First Commit

---
+++ src/main/java/co/edu/uniajc/estudiante/opemay/OpemayApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniajc.estudiante.opemay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpemayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpemayApplication.class, args);
+	}
+
+}
