d0da6d5ae194df8007e82aaafcf15640dacc8b37
canarvaezis <criz.narvaez21@gmail.com>
2025-09-08 20:33:12 -0500

    Primer commit del proyecto OpemAyShopFruit

---
+++ src/main/java/co/edu/uniajc/estudiante/opemayfruitshop/OpemAyFruitShopApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniajc.estudiante.opemayfruitshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpemAyFruitShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpemAyFruitShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/uniajc/estudiante/opemayfruitshop/OpemAyFruitShopApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uniajc.estudiante.opemayfruitshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpemAyFruitShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
